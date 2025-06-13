1c61a855529d33e2160e5c8fa7c510bb86fd0db6
Nora Schinkel <ncschinkel@gmail.com>
2025-06-13 17:01:00 +0200

    Separate bools and enums form the other inputs for validation and better handling later on

--- src/pyetm/models/input.py
+++ src/pyetm/models/input.py
@@ -3,32 +3,49 @@ from pydantic import BaseModel
 
 class Input(BaseModel):
     key: str
-    # These datatypes depend on Unit! - we might need an extra model for it,
-    # that groups unit types with min/max options for validation. For for example
-    # "enum" units there is no min or max, but these will have a
-    # "permitted_values" field instead.
-    # Idea: we need subtypes of Input - like EnumInput and BoolInput and FloatInput
-    min: Optional[float] = None
-    max: Optional[float] = None
     unit: str
     user: Optional[float | str | bool] = None
     disabled: Optional[bool] = False
     coupling_disabled: Optional[bool] = False
     coupling_groups: Optional[list[str]] = []
     disabled_by: Optional[str] = None
-    share_group: Optional[str] = None
-    step: Optional[float] = None
 
     @classmethod
     def from_json(cls, data: tuple[str, dict]):
         '''Initialise an Input from a JSON-like tuple object coming from .items()'''
         key, data = data
         data.update({'key': key})
-        return cls(**data)
+        klass = cls.class_type(data["unit"])
+        return klass(**data)
 
+    @staticmethod
+    def class_type(unit):
+        if unit == "bool":
+            return BoolInput
+        elif unit == "enum":
+            return EnumInput
+        else:
+            return FloatInput
 
     # Nice to have:
-    # Add validation for input GET - which fields are sometimes must and which are (always) optional?
     # Add validation for input SET - correct units, between min and max
 
 
+class BoolInput(Input):
+    ''' Input representing a boolean '''
+    user: Optional[bool] = None
+
+
+class EnumInput(Input):
+    ''' Input representing an enumarable '''
+    user: Optional[str] = None
+    permitted_values: list[str]
+
+
+class FloatInput(Input):
+    ''' Input representing a float '''
+    user: Optional[float] = None
+    min: float
+    max: float
+    share_group: Optional[str] = None
+    step: Optional[float] = None
