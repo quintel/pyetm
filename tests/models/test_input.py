cb79411888a1d3b6439fb60167abb3462410d297
louispt1 <louisparkestalbot1@gmail.com>
2025-06-20 10:56:21 +0200

    Add defaults to input model and fetch_inputs_example

--- tests/models/test_input.py
+++ tests/models/test_input.py
@@ -2,9 +2,10 @@ import pytest
 
 from pyetm.models import Input
 
+
 @pytest.mark.parametrize(
-    'json_fixture',
-    ['float_input_json', 'enum_input_json', 'bool_input_json', 'disabled_input_json']
+    "json_fixture",
+    ["float_input_json", "enum_input_json", "bool_input_json", "disabled_input_json"],
 )
 def test_input_from_json(json_fixture, request):
     input = Input.from_json(next(iter(request.getfixturevalue(json_fixture).items())))
