4b0de76d688ceb9b1d64de325234db867b0b9867
Nora Schinkel <ncschinkel@gmail.com>
2025-07-29 16:50:10 +0200

    Add validators for inputs

--- tests/models/test_input.py
+++ tests/models/test_input.py
@@ -1,5 +1,6 @@
 import pytest
 from pyetm.models import Input
+from pyetm.models.inputs import BoolInput, EnumInput
 
 
 @pytest.mark.parametrize(
@@ -12,3 +13,58 @@ def test_input_from_json(json_fixture, request):
 
     # Assert valid input
     assert input
+
+
+def test_bool_input():
+    input = BoolInput(key='my_bool', unit='bool', default=0.0)
+
+    # Setting the input
+    input.user = 0.0
+    assert input.user == 0.0
+
+    # Is it valid to update to string?
+    validity = input.is_valid_update('true')
+    assert len(validity) > 0
+    assert 'user: Input should be a valid number, unable to parse string as a number' in validity
+
+    # Is it valid to update to 0.5?
+    validity = input.is_valid_update(0.5)
+    assert len(validity) > 0
+    assert 'user: Value error, 0.5 should be 1.0 or 0.0 representing True/False, or On/Off' in validity
+
+    # Try to update to 0.5
+    input.user = 0.5
+    assert input.user == 0.0
+
+    # Reset the input
+    input.user = "reset"
+    assert input.user is None
+
+    # TODO: @Louis OLD warnings persist!
+    assert len(input.warnings) == 0
+
+def test_enum_input():
+    input = EnumInput(
+        key='my_enum',
+        unit='enum',
+        default='diesel',
+        permitted_values=['diesel', 'gasoline']
+    )
+
+    # Setting the input
+    input.user = 'gasoline'
+    assert input.user == 'gasoline'
+
+    # TODO: @Louis ValidationError is not caught in .warnings?? why?
+    # Is it valid to update to kerosene?
+    validity = input.is_valid_update('kerosene')
+    assert len(validity) > 0
+    assert "user: Value error, kerosene should be in ['diesel', 'gasoline']" in validity
+
+    # Try to update to 0.5
+    input.user = 0.5
+    assert input.user == 'gasoline'
+
+    # Reset the input
+    input.user = "reset"
+    assert input.user is None
